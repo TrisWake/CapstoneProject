7b881e5ecd3f0ffd2c67e52435b135fa48573fdd
Tristan Wakefield <tristan.wakefield@yahoo.com>
2024-12-11 10:18:24 -0600

    partial

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-const userlocation = document.querySelector('#location')
-const startDateInput = document.querySelector('#start-date')
-const endDateInput = document.querySelector('#end-date')
+const userLocation = document.querySelector('#location')
 const searchButton = document.querySelector('#search')
 searchButton.addEventListener('click', ()=>{
-    const location = userlocation.value
-    const startDate = startDateInput.value
-    const endDate = endDateInput.value
-    const URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_Key}&q=${userlocation.value}&dt=${startDate}&end_dt=${endDate}`
+    const location = userLocation.value
+    const URL = `http://api.weatherapi.com/v1/current.json?key=${API_Key}&q=${location}&days=3&aqi=no&alerts=no`
     fetch(URL)
     .then(function(rawResponse) {
         return rawResponse.json()
@@ -14,16 +10,16 @@ searchButton.addEventListener('click', ()=>{
     .then(function(data){
         console.log(data)
         const forecastContainer = document. querySelector('#forecast-container')
-        forecastContainer.innerHTML = ""
-        // data.forecastday.
         let weatherImage = ""
-        switch (data.forecastday.day.condition.text) {
+        switch (data.current.condition.text) {
             case "Cloudy":
                 weatherImage = cloudy
                 break
             case "Partly Cloudy":
                 weatherImage = partlyCloudy
                 break
+                case "Rain":
+                    weatherImage = rainy
             case "Sunny":
                 weatherImage = sunny
                 break
@@ -41,15 +37,16 @@ searchButton.addEventListener('click', ()=>{
     if (weatherImageElement) {
         weatherImageElement.src = weatherImage
     }
-    const temperature = data.forecastday[0].day.temp_f
-    const condition = data.forecastday[0].day.condition.text
+    const temperature = data.current.temp_f
+    const condition = data.current.condition.text
 
     const weatherInfo = document.querySelector('#weather-info')
     if (weatherInfo) {
-        weatherInfo.innerHTML = `Current weather: ${condition}, Temperature: ${temperature}°C`;
+        weatherInfo.innerHTML = `Current weather: ${condition}, Temperature: ${temperature}°F`;
     }
     })
 })
+
 //display current weather and forecast for dates
 // style page (background, bigger weather image, )
 //add music per weather condition
--- weatherIcons.js
+++ weatherIcons.js
@@ -1,5 +1,6 @@
 const cloudy = "//cdn.weatherapi.com/weather/64x64/night/119.png"
 const partlyCloudy = "//cdn.weatherapi.com/weather/64x64/day/116.png"
+const rainy = "//cdn.weatherapi.com/weather/64x64/day/296.png"
 const sunny = "//cdn.weatherapi.com/weather/64x64/day/113.png"
 const clear = "//cdn.weatherapi.com/weather/64x64/night/113.png"
 const mist = "//cdn.weatherapi.com/weather/64x64/night/143.png"
