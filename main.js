6c0980539264eec50adfaab306da31e1263ed9b7
Tristan Wakefield <tristan.wakefield@yahoo.com>
2024-12-12 10:41:49 -0600

    partial

--- main.js
+++ main.js
@@ -2,62 +2,50 @@ const userLocation = document.querySelector('#location')
 const searchButton = document.querySelector('#search')
 searchButton.addEventListener('click', ()=>{
     const location = userLocation.value
-    const URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_Key}&q=${location}&days=3&aqi=no&alerts=no`
+    const URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_Key}&q=${location}&days=3&aqi=no`
     fetch(URL)
     .then(function(rawResponse) {
         return rawResponse.json()
     })
     .then(function(data){
         console.log(data)
-        const forecastContainer = document. querySelector('#forecast-container')
-        let weatherSong = ""
-        let weatherImage = ""
-        switch (data.forecast.forecastday[i].day.condition.text) {
-            case "Cloudy":
-                weatherImage = cloudy
-                weatherSong = cloudySong
-                break
-            case "Partly Cloudy":
-                weatherImage = weatherpartlyCloudy
-                weatherSong = partlyCloudySong
-                break
-                case "Rain":
-                weatherImage = rain
-                weatherSong = rainySong
-                break
-            case "Sunny":
-                weatherImage = sunny
-                weatherSong = sunnySong
-                break
-            case "Clear":
-                weatherImage = clear
-                weatherSong = clearSong
-                break
-            case "Mist":
-                weatherImage = mist
-                weatherSong = mistSong
-                break
-            default:
-                weatherImage = clear
-                break
-        }
-    const weatherImageElement = document.querySelector('#weather-image')
-    if (weatherImageElement) {
-        weatherImageElement.src = weatherImage
-    }
-    const temperature = data.forecast.forecastday.day.temp_f
-    const condition = data.forecast.forecastday.day.condition.text
+        console.log(data.forecast.forecastday[0].day["avgtemp_f"])
+        console.log(data.forecast.forecastday[1].day["avgtemp_f"])
+        let currentWeather = document.querySelector("#weather-info")
+        currentWeather.innerText = data.forecast.forecastday[0].day["avgtemp_f"]
+        
+        
+        let weatherImage = document.querySelector('#weather-image')
+        let image = data.forecast.forecastday[0].day
+        if(data.forecast.forecastday[0].day.condition.text = "sunny"){
+            weatherImage.src = sunny && 
+        } 
+        if(data.forecast.forecastday[0].day.condition.text = "cloudy"){
+            weatherImage.src = cloudy
+        } 
+        if(data.forecast.forecastday[0].day.condition.text = "rain"){
+            weatherImage.src = rain
+        } 
+        if(data.forecast.forecastday[0].day.condition.text = "mist"){
+            weatherImage.src = mist
+        } 
+        if(data.forecast.forecastday[0].day.condition.text = "partly cloudy"){
+            weatherImage.src = partlyCloudy
+        } 
+        if(data.forecast.forecastday[0].day.condition.text = "clear"){
+            weatherImage.src = clear
+        } 
+    })
 
-    const weatherInfo = document.querySelector('#weather-info')
-    if (weatherInfo) {
-        weatherInfo.innerHTML = `Current weather: ${condition}, Temperature: ${temperature}°F`;
-    }
+    // const weatherInfo = document.querySelector('#weather-info')
+    // if (weatherInfo) {
+    //     weatherInfo.innerHTML = `Current weather: ${condition}, Temperature: ${temperature}°F`;
+    // }
     const weatherAudio = document.querySelector('#weather-audio')
             const audioSource = document.querySelector('#audio-source')
             audioSource.src = weatherSong
             weatherAudio.load()
             weatherAudio.play()
-    })
 })
 
 
